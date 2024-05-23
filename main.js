a182f278691deade22cb380659aabe49e4921545
flott-live <flott.live@gmail.com>
2024-05-24 01:32:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // and load the index.html of the app.
+  win.loadURL('https://flott.live');
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
