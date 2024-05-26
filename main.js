5ed4335bbc2ffbf5a3eec5074d750d9cdba594c4
flott-live <flott.live@gmail.com>
2024-05-26 15:53:14 +0200

    windiw size

--- main.js
+++ main.js
@@ -3,10 +3,8 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    // width: 800,
-    // height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true
     }
